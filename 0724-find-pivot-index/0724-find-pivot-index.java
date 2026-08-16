dcd9ae372864fa80d4affc8856e068d259cb81a5
arsalnaye <arsalnaye@gmail.com>
2026-08-17 00:51:14 +0530

    Time: 1 ms (97.57%), Space: 47.5 MB (40.97%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,20 +1,21 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int n=nums.length;
-        int lfsum[]=new int[n];
-        int rtsum[]=new int[n];
-        lfsum[0]=nums[0];
-        for(int i=1;i<n;i++){
-            lfsum[i]=lfsum[i-1]+nums[i];
+        int n = nums.length;
+        int lfsum[] = new int[n];
+        int rtsum[] = new int[n];
+        lfsum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            lfsum[i] = lfsum[i - 1] + nums[i];
         }
-        rtsum[n-1]=nums[n-1];
-        for( int i=n-2;i>=0;i--){
-            rtsum[i]=rtsum[i+1]+nums[i];
+        rtsum[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rtsum[i] = rtsum[i + 1] + nums[i];
         }
-        for(int i=0;i<n;i++){
-            if(lfsum[i]==rtsum[i]){
+        for (int i = 0; i < n; i++) {
+            if (lfsum[i] == rtsum[i]) {
                 return i;
             }
-        }return -1;
+        }
+        return -1;
     }
 }
