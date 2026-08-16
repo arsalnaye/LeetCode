1e0ece3179cf0fcdc5cb6a43e1a9ae96e7370897
arsalnaye <arsalnaye@gmail.com>
2026-08-17 02:02:09 +0530

    Time: 1 ms (98.15%), Space: 42.8 MB (23.81%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+
+        int ans = 1;
+        int s = 1, e = x;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (mid <= x / mid) {
+                ans = mid;
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
