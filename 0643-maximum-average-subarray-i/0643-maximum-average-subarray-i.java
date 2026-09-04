07fee4743f72d3228234437b19e7adbdd5515ec1
Arsalna <arsalnaye@gmail.com>
2026-09-04 17:53:07 +0530

    Time: 5 ms (32.09%), Space: 69.8 MB (25.79%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -6,10 +6,10 @@ class Solution {
             sum=sum+nums[i];
             }
             avg=sum/k;
-        max=avg;
+        max=avg;// isliye kyuki numbers agar negative hue and avg becomes -ve---> we did Math.max then max will be 0 instead of -ve average
         for(int i=k;i<nums.length;i++){
             //sum=sum+nums[i];
-            sum=sum+nums[i]-nums[i-k];
+            sum=sum+nums[i]-nums[i-k];// aage wala num window ke andar(add) aur peeche wala bahar(remove)==> windown slide
             avg=(sum/k);
             max=Math.max(max,avg);
         }return max;
